d86a4ae7d3aae81862553935f7d602a0730d85d1
vianxkkkkj <00001126366006sp@al.educacao.sp.gov.br>
2024-10-17 20:40:28 -0300

    js

---
+++ skstch.js
@@ -0,0 +1,16 @@
+let cor;
+let posicaoHorzontal
+let posicaoVertical
+
+function setup(){
+ createCanvas(400, 400);
+  background(color(100, 0, 0));
+ cor = color(random(0, 255), random(0, 255),random(0 ,255)); 
+  posicaoHorizontal = 200;
+  posicaoVertical =200;
+}
+
+function draw() {
+fill(cor);
+circle(posicaoHorizontal, posicaoVertical, 50);
+} 
